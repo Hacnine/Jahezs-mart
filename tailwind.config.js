02da5ecad9bcc7c7be7547809b19b82ace709cd1
Hacnine <hacninetusar@gmail.com>
2023-09-19 09:49:19 +0600

    It's shopping site

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:"'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      }
+    },
+    screens:{
+      xl:'1200px',
+       md:'768px', sm:'576px',
+       lg:'992px',
+    },
+    container:{
+      center:true,
+      padding: '1rem'
+    },
+    // colors:{
+    //   'primary': '#FF5733',
+    //   'secondary': '#ff0000',
+    //   'white': '#f3e8ff'
+    // }
+  },
+  plugins: [],
+}
+
